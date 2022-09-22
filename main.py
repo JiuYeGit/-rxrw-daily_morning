d99e2b31db2f9519202cc10dfc06a156b46fde69
fish <49136603+JiuYeGit@users.noreply.github.com>
2022-09-22 10:18:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ wm = WeChatMessage(client)
 wea, temperature = get_weather()
 data = {
 "weather":{"value":wea},"temperature":{"value":temperature},
-"love_days":{"value":get_count()}, "color":get_random_color()},
+"love_days":{"value":get_count()}, "color":{get_random_color()},
 "date_new":{"value":date_new},"holiday":{"value":holiday},
 "soup":{"value":get_du()}, "writing":{"value":get_pyq()}
 }
